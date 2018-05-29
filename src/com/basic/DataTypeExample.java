4f47837a2f9a9d13cee37b00b7982e8f5b8f49b7
pavikumar <pavithara810@gmail.com>
2018-05-29 15:59:37 +0530

    sample code

--- src/com/basic/DataTypeExample.java
+++ src/com/basic/DataTypeExample.java
@@ -2,6 +2,11 @@ package com.basic;
 
 public class DataTypeExample {
 public static void main(String[] args) {
+	int count=120;
+	count++;
+	count--;
+	System.out.println(count!=250);
+	System.out.println(count==200);
 	
 }
 }
